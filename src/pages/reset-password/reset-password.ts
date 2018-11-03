67c2ff0c3ba12ef9be16b25ca69197caa6e16cb1
Vitória Nadejda <vi.nadejda98@gmail.com>
2018-11-03 20:58:33 -0300

    Pagina para a redefinição de senha

---
+++ src/pages/reset-password/reset-password.ts
@@ -0,0 +1,81 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
+
+/**
+ * Generated class for the ResetPasswordPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-reset-password',
+  templateUrl: 'reset-password.html',
+})
+export class ResetPasswordPage {
+
+  @ViewChild('email') email;
+
+  public resetPwdForm: any;
+  messageEmail: string = "";
+  errorEmail: boolean = false;
+
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    public formBuilder: FormBuilder,
+    public firebaseAuth: AngularFireAuth
+  ) {
+    this.resetPwdForm = formBuilder.group({
+      email: [
+        '',
+        Validators.required
+      ]
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ResetPasswordPage');
+  }
+
+  public dataValidade(): void {
+    let { email } = this.resetPwdForm.controls;
+    if (!this.resetPwdForm.valid) {
+      if (!email.valid) {
+        this.errorEmail = true;
+        this.messageEmail = "Email inválido!";
+      }
+      else {
+        this.messageEmail = "";
+      }
+    }
+    else {
+      this.resetPassword();
+    }
+  }
+
+  public resetPassword() {
+    this.firebaseAuth.auth.sendPasswordResetEmail(this.email.value)
+      .then(() => {
+        this.showMessage('Um email foi enviado para a redefinição de senha.');
+        this.navCtrl.setRoot(LoginPage);
+      })
+      .catch((erro: any) => {
+        this.showMessage(erro);
+      });
+  }
+
+  private showMessage(mensagem: string): void {
+    let toast = this.toastCtrl.create(
+      {
+        duration: 3000,
+        position: 'botton'
+      }
+    );
+    toast.setMessage(mensagem);
+    toast.present();
+  }
+}
